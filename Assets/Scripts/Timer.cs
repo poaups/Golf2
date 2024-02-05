23d817bf32af543e96eff16f4f454e64a3725d6f
Popey0424 <roylouis0424@gmail.com>
2024-02-05 23:30:15 +0100

    Je push ca mais bon on vas devoir voir demain

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class Timer : MonoBehaviour
 {
+    public Animator animator;
+    private float hasPlayed = 0;
+    public GameCore[] gameCore;
+    public Image imageFade;
+    public GameObject spwaner;
+
     [Header("Component")]
     public TextMeshProUGUI timerText;
+    public TextMeshProUGUI timerAnimation;
 
     [Header("TimerSettings")]
     public float currentTime;
@@ -16,6 +24,7 @@ public class Timer : MonoBehaviour
     [Header("LimitSettings")]
     public bool hasLimit;
     public float timerLimit;
+    public float timerAlarm;
 
     [Header("FormatSettings")]
     public bool hasFormat;
@@ -27,6 +36,7 @@ public class Timer : MonoBehaviour
         timeFormats.Add(TimerFormats.Whole, "0");
         timeFormats.Add(TimerFormats.TenthDecimal, "0.0");
         timeFormats.Add(TimerFormats.HundrethsDecimal, "0.00");
+        animator = GetComponent<Animator>();
 
     }
 
@@ -40,17 +50,61 @@ public class Timer : MonoBehaviour
             SetTimerText ();
             timerText.color = Color.red;
             enabled = false;
+            imageFade.DOFade(1, 2.9f).OnComplete(FadeComplete);
         }
         
         SetTimerText();
+        if (currentTime < timerAlarm && hasPlayed == 0)
+        {
+            Debug.Log("ehfsieufhseuf");
+            animator.SetTrigger("Move");
+            hasPlayed = 1;
+            
+        }
     }
 
     private void SetTimerText()
     {
         timerText.text = hasFormat ? currentTime.ToString(timeFormats[format]) : currentTime.ToString();
     }
+    
+    public void FadeComplete()
+    {
+        spwaner.GetComponent<Spawner_Ball>().Reset_Position();
+        ResetTimer();
+
+    }
+    public void ResetTimer()
+    {
+        Update();
+    }
 
     
+
+    //public void MoveText()
+    //{
+    //    if (currentTime <= timerAlarm)
+    //    {
+    //        Animator animator = timerText.GetComponent<Animator>();
+    //        if (animator != null)
+    //        {
+    //            bool isMove = animator.GetBool("Move");
+
+    //            animator.SetBool("Move", !isMove);
+    //        } 
+            
+
+
+
+
+
+
+
+
+    //    }
+    //}
+
+
 }
 
 public enum TimerFormats
